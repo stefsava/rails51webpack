6c928e4a0c30007855508b395c3683d127e18c41
Geremia Taglialatela <tagliala.dev@gmail.com>
2016-12-21 16:36:07 +0100

    Use the object spread operator
    Enable the object spread operator proposal to write { ...state,
    ...newState } instead of using `Object.assign()`

--- config/webpack/shared.js
+++ config/webpack/shared.js
@@ -22,7 +22,8 @@ module.exports = {
         exclude: /node_modules/,
         loader: 'babel-loader',
         query: {
-          presets: ['react', 'es2015']
+          presets: ['react', 'es2015'],
+          plugins: ['transform-object-rest-spread']
         }
       }
     ]
