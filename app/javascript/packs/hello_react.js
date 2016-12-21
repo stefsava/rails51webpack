340cd31517638661e61879cf001897d542f98567
Geremia Taglialatela <tagliala.dev@gmail.com>
2016-12-21 16:29:30 +0100

    Use an existent DOM element as domContainerNode

--- app/javascript/packs/hello_react.js
+++ app/javascript/packs/hello_react.js
@@ -27,6 +27,6 @@ document.addEventListener("DOMContentLoaded", e => {
     <Provider store={store}>
       <App />
     </Provider>,
-    document.body.appendChild(document.createElement('div'))
+    document.getElementById('root')
   )
 })
