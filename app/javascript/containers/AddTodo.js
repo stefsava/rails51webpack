229feb681688bea68d047b6e8d78827cc41c0d66
Geremia Taglialatela <tagliala.dev@gmail.com>
2017-01-03 16:37:08 +0100

    Change object-curly-spacing linter to always

--- app/javascript/containers/AddTodo.js
+++ app/javascript/containers/AddTodo.js
@@ -1,8 +1,8 @@
 import React from "react"
-import {connect} from "react-redux"
-import {addTodo} from "../actions"
+import { connect } from "react-redux"
+import { addTodo } from "../actions"
 
-let AddTodo = ({dispatch}) => {
+let AddTodo = ({ dispatch }) => {
   let input
 
   return (
--- app/javascript/packs/hello_react.js
+++ app/javascript/packs/hello_react.js
@@ -16,9 +16,9 @@
 // })
 
 import React from "react"
-import {render} from "react-dom"
-import {createStore} from "redux"
-import {Provider} from "react-redux"
+import { render } from "react-dom"
+import { createStore } from "redux"
+import { Provider } from "react-redux"
 import App from "../components/App"
 import reducer from "../reducers"
 
