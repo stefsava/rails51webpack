95115f720e55b5a1ac26b3ffb62402b9934a555a
Stefano Savanelli <stefano@savanelli.it>
2017-01-04 15:11:38 +0100

    add ButtonToolbar

--- app/javascript/components/Footer.js
+++ app/javascript/components/Footer.js
@@ -1,11 +1,11 @@
 import React from "react"
 import FilterLink from "../containers/FilterLink"
-import { Link } from "react-router"
+import { Link, IndexLink } from "react-router"
 import { LinkContainer } from "react-router-bootstrap"
-import { Button } from "react-bootstrap"
+import { Button, ButtonToolbar, ButtonGroup, Glyphicon } from "react-bootstrap"
 
 const Footer = () => (
-  <p>
+  <div>
     Show:
     {" "}
     <FilterLink filter="SHOW_ALL">
@@ -19,20 +19,44 @@ const Footer = () => (
     <FilterLink filter="SHOW_COMPLETED">
       Completed
     </FilterLink>
-
     <br />
     <Link to="/">Uno</Link>
     <Link to="/pippo">Due</Link>
 
     <br />
-    <LinkContainer to={{ pathname: "/", query: { bar: "baz" } }}>
-      <Button>Uno</Button>
-    </LinkContainer>
+    <IndexLink to="/">Uno</IndexLink>
+    <IndexLink to="/pippo">Due</IndexLink>
 
-    <LinkContainer to={{ pathname: "/pippo", query: { bar: "baz" } }}>
-      <Button>Due</Button>
-    </LinkContainer>
-  </p>
+    <ButtonToolbar>
+      <LinkContainer to={{ pathname: "/", query: { bar: "baz" } }}>
+        <Button>Uno</Button>
+      </LinkContainer>
+
+      <LinkContainer to={{ pathname: "/pippo", query: { bar: "baz" } }}>
+        <Button>Due</Button>
+      </LinkContainer>
+    </ButtonToolbar>
+
+    <ButtonToolbar>
+      <ButtonGroup>
+        <Button><Glyphicon glyph="align-left" /></Button>
+        <Button><Glyphicon glyph="align-center" /></Button>
+        <Button><Glyphicon glyph="align-right" /></Button>
+        <Button><Glyphicon glyph="align-justify" /></Button>
+      </ButtonGroup>
+    </ButtonToolbar>
+    <ButtonToolbar>
+      <ButtonGroup>
+        <Button bsSize="large"><Glyphicon glyph="star" /> Star</Button>
+        <Button><Glyphicon glyph="star" /> Star</Button>
+        <Button bsSize="small"><Glyphicon glyph="star" /> Star</Button>
+        <Button bsSize="xsmall"><Glyphicon glyph="star" /> Star</Button>
+      </ButtonGroup>
+    </ButtonToolbar>
+
+
+
+  </div>
 )
 
 export default Footer
