5064b7f1dfddd6bc27ec716ae12986962ad4f1c7
Stefano Savanelli <stefano@savanelli.it>
2017-01-04 12:10:42 +0100

    add react-router-bootstrap support and sample code

--- app/javascript/components/Footer.js
+++ app/javascript/components/Footer.js
@@ -1,6 +1,8 @@
 import React from "react"
 import FilterLink from "../containers/FilterLink"
 import { Link } from "react-router"
+import { LinkContainer } from "react-router-bootstrap"
+import { Button } from "react-bootstrap"
 
 const Footer = () => (
   <p>
@@ -17,9 +19,19 @@ const Footer = () => (
     <FilterLink filter="SHOW_COMPLETED">
       Completed
     </FilterLink>
+
     <br />
     <Link to="/">Uno</Link>
     <Link to="/pippo">Due</Link>
+
+    <br />
+    <LinkContainer to={{ pathname: "/", query: { bar: "baz" } }}>
+      <Button>Uno</Button>
+    </LinkContainer>
+
+    <LinkContainer to={{ pathname: "/pippo", query: { bar: "baz" } }}>
+      <Button>Due</Button>
+    </LinkContainer>
   </p>
 )
 
